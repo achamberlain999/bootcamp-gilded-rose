fe58e0f1b6e291c5d21456c1c7a30b6fe6438fe7
Hashir Majeed <hashir.majeed@softwire.com>
2023-07-04 10:55:32 +0100

    second test passes

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -7,3 +7,13 @@ describe('Gilded Rose', () => {
     expect(items[0].name).toBe('foo');
   });
 });
+
+describe('Gilded Rose', () => {
+  it('quality degrades faster after sell date', () => {
+    const gildedRose = new GildedRose([new Item('foo', 2, 10)]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(6);
+  });
+});
